8380f4e90a50d14e04857024e56c10d547bdb79f
Remi Morisawa <morisawaremi@gmail.com>
2024-11-07 10:38:15 +0900

    Add for loop to repeat string based on input number

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(text, number) {
+  let string = "";
+  for(let i = 0; i < number; i++) {
+    string += text;
+  }
+  return string;
 };
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
