f94fbb561dd28134c1d826562311a0dfdffa33b6
Remi Morisawa <morisawaremi@gmail.com>
2024-11-07 11:46:40 +0900

    Add getRandomNumber function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,11 +1,19 @@
 const repeatString = function(text, number) {
   let string = "";
+  let error = "ERROR";
+  if(number < 0) {
+  return error;
+  }
   for(let i = 0; i < number; i++) {
     string += text;
   }
   return string;
 };
-repeatString('hey', 3);
+function getRandomNumber () {
+  return number;
+}
+//I'm unsure why calling getRandomNumber passes the test only without the () here
+repeatString('odin', getRandomNumber);
 
 // Do not edit below this line
 module.exports = repeatString;
