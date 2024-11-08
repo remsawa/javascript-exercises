094837088765def88ad9cc7f60eb3dbc7c7d393a
Remi Morisawa <morisawaremi@gmail.com>
2024-11-08 11:33:24 +0900

    Add .splice to removeFromArray's array argument

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+  let split = text.split('');
+  let reverse = split.reverse();
+  let join = split.join('');
+  return join;
 };
-
+reverseString("")
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, remove) {
+  const removedItem = remove - 1;//2
+  array.splice(removedItem, 1);//(2, 1)
+  return array;
 };
+removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
