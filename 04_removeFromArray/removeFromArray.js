8f4a40a21be5551392600ae9b55f9cfce9c5a1d2
Remi Morisawa <morisawaremi@gmail.com>
2024-11-12 14:15:55 +0900

    Add filter() and includes() to newArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,35 @@
+function removeFromArray(array, ...removeItem) { 
+  const newArray = array.filter(arrayItem => !removeItem.includes(arrayItem));
+  return newArray;
+}
+
+removeFromArray([1, 2, 3, 4], 3, 2);
+
+/*
+
+
+
+(3) [1, 2, 4]
 const removeFromArray = function(array, remove) {
-  const removedItem = remove - 1;//2
-  array.splice(removedItem, 1);//(2, 1)
-  return array;
+    const removedItem = remove - 1;//2
+    array.splice(removedItem, 1);//(2, 1)
+    return array;
 };
 removeFromArray([1, 2, 3, 4], 3);
+
+
+
+
+
+=====
+
+
+
+
+const myArray = [1, 2, 3, 4];
+myArray.splice(2, 1);
+console.log(myArray);//[1,2,4]
+*/
 
 // Do not edit below this line
 module.exports = removeFromArray;
