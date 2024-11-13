ee9f0b759786eaac4c2f51ed08822f0cf010a2c4
Remi Morisawa <morisawaremi@gmail.com>
2024-11-13 12:19:45 +0900

    Add boolean to check if int1 and int2 are negative numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -4,8 +4,11 @@ const sumAll = function(int1, int2) {
   let lesserInt
   let greaterInt
   const lesserGreaterArray = [lesserInt, greaterInt];
-  if (typeof int1 != 'number' || typeof int2 != 'number') {
-      console.log('ERROR');
+  if (typeof int1 != 'number' ||
+      typeof int2 != 'number' ||
+      int1 < 0 ||
+      int2 < 0) {
+      return 'ERROR';
   } else {
      if (int1 > int2) {
          lesserGreaterArray[1] = int1;
