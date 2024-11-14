c2693da1ffc41e6025261bf1ee11b06a73061140
Remi Morisawa <morisawaremi@gmail.com>
2024-11-14 10:16:48 +0900

    Add boolean to check for century years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -2,15 +2,14 @@ const leapYears = function(year) {
   let divBy4 = year % 4 === 0;
   let divBy100 = year % 100 === 0;
   let divBy400 = year % 400 === 0
-  if (divBy4 ||
+  if (divBy4 && !divBy100 ||
       divBy4 && divBy100 && divBy400) {
       return true;
-  } else if (!divBy4 ||
-             divBy100) {
+  } else {
     return false;
   }
 }
-leapYears(2000);
+leapYears(1900);
 
 // Do not edit below this line
 module.exports = leapYears;
