7dfe5cd93763c4db5fc1589a4b646d451c2ecf9e
Remi Morisawa <morisawaremi@gmail.com>
2024-11-14 10:56:41 +0900

    Add C to F formula and Math.round to round to the tenth place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,16 @@
 const convertToCelsius = function(degreeF) {
-  return Math.round((degreeF -32 * (5/9)) / 100);
+  let convertedDegreeC = (degreeF -32) * (5/9);
+   return Math.round(convertedDegreeC * 10) / 10;
+   
 };
 
-const convertToFahrenheit = function(degreeC) {
+const convertToFahrenheit = function(degreeF) {
+   let convertedDegreeF = (degreeF * (9/5) + 32);
+   return Math.round(convertedDegreeF * 10) / 10;
 };
 
-convertToCelsius(32);
+convertToCelsius(100);
+convertToFahrenheit(0);
 //convertToFahrenheit(0);
 
 // Do not edit below this line
