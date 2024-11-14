d36d01a3d0677111c9cb266ebe11eda78a3ebb22
Remi Morisawa <morisawaremi@gmail.com>
2024-11-14 10:36:12 +0900

    Add F to C formula and Math.round to round degree to the nearest tenth

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreeF) {
+  return Math.round((degreeF -32 * (5/9)) / 100);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeC) {
 };
+
+convertToCelsius(32);
+//convertToFahrenheit(0);
 
 // Do not edit below this line
 module.exports = {
