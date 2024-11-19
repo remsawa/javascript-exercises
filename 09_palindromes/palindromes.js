492b181c0fa9fd3796cc1f71a28791d32897f40c
Remi Morisawa <morisawaremi@gmail.com>
2024-11-19 10:21:14 +0900

    Pass all test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,12 +1,13 @@
 const palindromes = function (string) {
   string = string.toLowerCase();
-  const lettersOnlyStr = string.replace(/[.,\/#!$%\^&\*;:{}=\-_" "`~()]/g,"")
-  const stringArray = Array.from(lettersOnlyStr);
+  const noPuncStr = string.replace(/[.,\/#!$%\^&\*;:{}=\-_" "`~()]/g,"");
+  const noNumStr = noPuncStr.replace(/[0-9]/g, '');
+  const stringArray = Array.from(noPuncStr);
   const reverseStringArray= stringArray.toReversed();
   const joinReverseStrArr = reverseStringArray.join('');
-  console.log(lettersOnlyStr);
+  console.log(noNumStr);
   console.log(joinReverseStrArr);
-  if(lettersOnlyStr == joinReverseStrArr) {
+  if(noPuncStr == joinReverseStrArr) {
       return true; 
   } else {
       return false;
