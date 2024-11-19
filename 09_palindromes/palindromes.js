bc1595be1001749a69c26c611006fd04ada28a13
Remi Morisawa <morisawaremi@gmail.com>
2024-11-19 10:13:44 +0900

    Add empty string to list of things to avoid in string.replace

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,11 +1,11 @@
 const palindromes = function (string) {
   string = string.toLowerCase();
-  const lettersOnlyStr = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+  const lettersOnlyStr = string.replace(/[.,\/#!$%\^&\*;:{}=\-_" "`~()]/g,"")
   const stringArray = Array.from(lettersOnlyStr);
-  const flatStrArr = stringArray.flat();
   const reverseStringArray= stringArray.toReversed();
   const joinReverseStrArr = reverseStringArray.join('');
-  
+  console.log(lettersOnlyStr);
+  console.log(joinReverseStrArr);
   if(lettersOnlyStr == joinReverseStrArr) {
       return true; 
   } else {
@@ -14,7 +14,7 @@ const palindromes = function (string) {
   compareArrays();
 };
 
-palindromes('RACEcar');
+palindromes('A car, a man, a maraca.');
 
 // Do not edit below this line
 module.exports = palindromes;
