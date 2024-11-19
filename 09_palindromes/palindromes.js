b655bb3fe25983c41b71081e46a857fb2d811c28
Remi Morisawa <morisawaremi@gmail.com>
2024-11-19 10:06:57 +0900

    Add toLowerCase to string

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  string = string.toLowerCase();
+  const lettersOnlyStr = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+  const stringArray = Array.from(lettersOnlyStr);
+  const flatStrArr = stringArray.flat();
+  const reverseStringArray= stringArray.toReversed();
+  const joinReverseStrArr = reverseStringArray.join('');
+  
+  if(lettersOnlyStr == joinReverseStrArr) {
+      return true; 
+  } else {
+      return false;
+  }
+  compareArrays();
 };
+
+palindromes('RACEcar');
 
 // Do not edit below this line
 module.exports = palindromes;
