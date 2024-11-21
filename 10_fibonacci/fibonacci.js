050dc6ffc60b900f4069b31af0f90c3c4f074c1f
Remi Morisawa <morisawaremi@gmail.com>
2024-11-21 10:47:55 +0900

    Add loop and fibonacci array
    Added a loop to add the previous number in the array (num1) to the next number (num2).

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+  let answer;
+  const fibSeq = [0];
+  function makeFibSeq() {
+      let i = 1;
+      fibSeq.push(i);
+      
+      for(i; i < number; i++) {
+          let num1 = fibSeq[i-1];
+          let num2 = fibSeq[i];
+          let newNum = num1 + num2;
+          fibSeq.push(newNum);
+      }
+     
+  }
+  makeFibSeq();
+  console.log(fibSeq);
+  answer = fibSeq[fibSeq.length -1];
 
+  return answer;
 };
-
+fibonacci(6);
 // Do not edit below this line
 module.exports = fibonacci;
