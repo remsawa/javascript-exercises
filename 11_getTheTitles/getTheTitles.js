0a077002330a7cb46bc671d662c5f344941bcd5a
Remi Morisawa <morisawaremi@gmail.com>
2024-11-22 11:45:02 +0900

    Add map method to array to find title

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,20 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+return array.map((item) => item.title);
 };
+
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
+
+
+getTheTitles(books);
 
 // Do not edit below this line
 module.exports = getTheTitles;
